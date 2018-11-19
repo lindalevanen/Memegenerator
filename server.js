0ab38fc0c042c5259ffa5fa5522ae3514bc098a8
Levänen Linda <levanel2@cs.hut.fi>
2018-11-19 15:01:51 +0200

    Moved static file thing outside production condition

--- server.js
+++ server.js
@@ -6,17 +6,19 @@ const bodyParser = require('body-parser')
 const Multer  = require('multer')
 const UUID = require("uuid-v4");
 
-app.engine('html', require('ejs').renderFile);
-app.set('view engine', 'html');
+// Serve any static files
+app.use(express.static(path.join(__dirname, "client/build")));
+
 
 if (process.env.NODE_ENV === "production") {
-  // Serve any static files
-  app.use(express.static(path.join(__dirname, "client/build")));
   // Handle React routing, return all requests to React app
   app.get("*", function(req, res) {
     res.sendFile(path.join(__dirname, "client/build", "index.html"));
   })
 }
+
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'html');
 
 /* To handle file upload */
 const mult = Multer({
